40b4f9599914aa2fe10150f1afedd67bf7499cee
Joe Shepherd <joeshepmedia@gmail.com>
2013-11-08 12:04:03 -0600

    Add fadeOut and click event to close lightbox

--- app/assets/coffeescript/read-more.coffee
+++ app/assets/coffeescript/read-more.coffee
@@ -19,14 +19,22 @@ $ ->
           $(this).text('Read less')
         false
 
-
+      // For added user-friendliness I added a click function to close the lightbox. Also threw in a fadeOut for a smoother transition before removing from the DOM.
+      // Made closing the image a separate function for DRY purposes.
       lightbox : $('.images-container').on 'click', 'img', ->
         image = $('<img/>').addClass('lightboxImage').attr('src', $(this).attr('src'))
         lightboxContainer = $('<div/>').addClass('lightbox').append(image)
 
         $('body').append(lightboxContainer).on 'keyup', (e) ->
           if e.keyCode is 13 or e.keyCode is 27
-            $('.lightbox').remove()
+            closePic()
+
+        $('.lightbox').on 'click', ->
+          closePic()
+
+        closePic = ->
+          $(".lightbox").fadeOut "normal", ->
+            $(this).remove()
 
     }
 
--- public/js/read-more.js
+++ public/js/read-more.js
@@ -20,15 +20,23 @@
           return false;
         }),
         lightbox: $('.images-container').on('click', 'img', function() {
-          var image, lightboxContainer;
+          var closePic, image, lightboxContainer;
 
           image = $('<img/>').addClass('lightboxImage').attr('src', $(this).attr('src'));
           lightboxContainer = $('<div/>').addClass('lightbox').append(image);
-          return $('body').append(lightboxContainer).on('keyup', function(e) {
+          $('body').append(lightboxContainer).on('keyup', function(e) {
             if (e.keyCode === 13 || e.keyCode === 27) {
-              return $('.lightbox').remove();
+              return closePic();
             }
           });
+          $('.lightbox').on('click', function() {
+            return closePic();
+          });
+          return closePic = function() {
+            return $(".lightbox").fadeOut("normal", function() {
+              return $(this).remove();
+            });
+          };
         })
       };
     })();
